c24ddcb22ff01299804a59323b6e23a5f4f63c70
root <root@localhost.localdomain>
2019-10-18 13:33:17 -0700

    correción end

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__, template_folder='')
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
 
 @app.route('/<path:path>')
